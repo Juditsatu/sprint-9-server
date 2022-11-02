3ef094b985b68cfaf4eaec8c457bb4f26bc59f28
Judisatu <judit.sant@gmail.com>
2022-11-02 20:17:40 +0100

    user edit form
    need http request

--- controllers/user.js
+++ controllers/user.js
@@ -20,17 +20,62 @@ const viewAllUsers = async (req, res = response) => {
 const viewUserById = async (req, res = response) => {
 
     try {
-        const all = await User.findById({ _id });
+        const all = await User.find({});
         res.send(all);
     } catch (error) {
-        return res.status(500).json({
-            ok: false,
-            msg: "No users found"
+        return res.status(500).json({ 
+            ok: false, 
+            msg: "No users found" 
         });
     }
 
 };
 
+const editUser = async (req, res = response, next) => {
+
+    User.findById(req.user.id, function (err, user) {
+        if (!user) {
+            return res
+                .status(httpStatus.INTERNAL_SERVER_ERROR)
+                .json({ message: 'User is not found' });
+        }
+        const schema = Joi.object().keys({
+            name: Joi.string()
+                .min(4)
+                .max(10)
+                .required(),
+            email: Joi.string()
+                .email()
+                .required(),
+            address: Joi.string().required(),
+            postalCode: Joi.string().required(),
+            city: Joi.string().required(),
+        });
+        const { error, value } = Joi.validate(req.body, schema);
+        if (error && error.details) {
+            return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details })
+        }
+        const email = Helpers.lowerCase(req.body.email);
+        const name = Helpers.lowerCase(req.body.name);
+        const address = req.body.address;
+        const postalCode = req.body.postalCode;
+        const city = req.body.city;
+        user.set({
+            email: email,
+            name: name,
+            address: address,
+            postalCode: postalCode,
+            city: city
+        });
+        user.save(function (error, updatedUser) {
+            if (error) {
+                /* Handle it */
+            }
+        })
+    });
+    
+};
 
 
-module.exports = { viewAllUsers, viewUserById };
+
+module.exports = { viewAllUsers, viewUserById, editUser };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-const { viewAllUsers } = require('../controllers/user');
+const { viewAllUsers, editUser } = require('../controllers/user');
 
 const router = Router();
 
 //Ver todos los usuarios
 router.get( '/', viewAllUsers );
+
+//Ver todos los usuarios
+router.get( '/edit', validateJWT, editUser );
 
 
 module.exports = router;
